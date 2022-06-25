b58093b630e07b6a02a168b0a3c28c20ded9471a
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-06-25 22:14:07 +0700

    fix page

--- atbeach/lib/screens/destination_screen.dart
+++ atbeach/lib/screens/destination_screen.dart
@@ -108,6 +108,13 @@ class _DestinationScreenState extends State<DestinationScreen> {
               ),
             ],
           ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+            ),
+          ),
           SizedBox(
             width: double.infinity,
             child: MaterialButton(
